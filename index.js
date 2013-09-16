fc8d88100d95e57e5e45692f30fdd6e83a33609f
Nikolay Ivanushkin <nikolay.ivanushkin@inn.ru>
2013-09-16 15:35:13 +0400

    Removed npm-log

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var server  = require('./server')
   , program = require('commander');
 
 program
-  .version('0.1.0')
+  .version('0.1.1')
   .option('-p, --port [number]', 'Sets port on which the server will work', '8000')
   .option('-r, --root [path]', 'Sets the root from which the server will run')
   .option('-l, --logs', 'Logs writing flag')
